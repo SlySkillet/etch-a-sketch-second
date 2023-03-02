67fb0abd9f8b2906651cc68787198734ded259a6
Simon Conrad <simondbconrad@gmail.com>
2023-03-02 14:29:25 -0400

    create function to make grid with mouseover functionality

--- script.js
+++ script.js
@@ -7,9 +7,41 @@ const pixelDisplay = function () {
   output.innerHTML = gridSize;
 };
 
-pixelDisplay(); // writes Pixels: 'current slider value'
-
 slider.oninput = function () {
   gridSize = this.value; // stores number of grid cells in gridSize variable
   pixelDisplay(); // rewrite Pixels with ^^ value
+  makeGrid(gridSize);
 };
+
+// create grid
+let board = document.querySelector('.grid-container');
+let selectCells = board.querySelectorAll('div');
+
+const makeGrid = function (size) {
+  selectCells.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // creates layout
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numberOfCells = size ** 2; //create divs according to size
+  for (i = 0; i < numberOfCells; i++) {
+    let cell = document.createElement('div');
+    cell.style.backgroundColor = 'pink';
+    cell.addEventListener('mouseover', colorCell);
+    board.insertAdjacentElement('beforeend', cell);
+  }
+};
+
+// color functionality
+let color = 'black';
+
+const colorCell = function () {
+  this.style.backgroundColor = color;
+};
+
+// button functionality
+
+//INITIALIZE:
+
+makeGrid(gridSize);
+
+pixelDisplay(); // writes Pixels: 'current slider value'
